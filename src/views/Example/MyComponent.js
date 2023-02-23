a926c1bc3fac0dcb888c98525559a23919abcc1d
tuananh-sama <trantuananh.only1@gmail.com>
2023-02-23 15:52:21 +0700

    third commit

---
+++ src/views/Example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello my component</div>
+        )
+    }
+}
+
+export default MyComponent;
